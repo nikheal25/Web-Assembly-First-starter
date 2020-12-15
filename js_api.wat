8bc9952e84b7a8234565a041565aee9588a3d730
Nick <nikheal25@gmail.com>
2020-12-15 17:25:40 +1100

    Function call

--- js_api.wat
+++ js_api.wat
@@ -1,6 +1,15 @@
+;; This is comment
+;;
+
 (module
+(func $doubleNumber (param i32) (result i32)
+local.get 0
+local.get 0
+i32.add
+)
     (func $add (param i32) (param $p1 i32) (result i32)
     local.get 0
+    call 0
     local.get $p1
     i32.add
     )
--- js_forWASM.js
+++ js_forWASM.js
@@ -2,6 +2,6 @@ fetch("js_api.wasm")
   .then((response) => response.arrayBuffer())
   .then((bytes) => WebAssembly.instantiate(bytes))
   .then((results) => {
-    let finalResult = results.instance.exports.addFunctionWA(1, 773);
+    let finalResult = results.instance.exports.addFunctionWA(3, 10);
     console.log(finalResult);
   });
